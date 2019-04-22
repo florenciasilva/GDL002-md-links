9df60a6aa91a8c3fc19bace3fda409b2abbf2e5f
florenciasilva <hensgrej@gmail.com>
2019-04-22 10:30:16 -0500

    Add basic testing to fileExists and relativeToAbsolute

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,23 @@
-const mdLinks = require('../');
+const { fileExists, relativeToAbsolute } = require('../src/md-links');
 
 
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('fileExists', () => {
+  it('It should return true if the file exists', () => {
+    expect(fileExists('src/prueba.md')).toBe(true);
   });
-
+  it('It should return false if the file doesn\'t exists', () => {
+    expect(fileExists('prueba.md')).toBe(false);
+  });
 });
+
+describe('relativeToAbsolute', () => {
+  it('It should return an absolute path given the relative path', () => {
+    expect(relativeToAbsolute('prueba.md')).toBe('C:\\Proyectos\\GDL002-md-links\\prueba.md');
+  });
+});
+
+describe('fileIsMD', () => {
+  it('It shoukd return true if the file is .md', () => {
+    expect(fileIsMD(url)).toBe(true);
+  })
+})
